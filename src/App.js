f51d85defb8c74657ed08ac5fad1c655d418cc80
Sangeetha2906 <yenubarisangeetha@gmail.com>
2023-02-21 16:44:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Classcomponent from './Classcomponent'
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+    <Nav/>
+    <Routes>
+    	<Route path="/" element={<Home/>}/>
+    	<Route path="/about" element={<About/>}/>
+    	<Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
+
+     {/* <Classcomponent/>*/}
+    </div>
+  )
+}
+
+export default App;
